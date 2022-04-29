e9dedd3e04f16be1a800901f1e72ea9ac99a1eda
meszkos <michalik515@gmail.com>
2022-04-29 10:39:41 +0200

    Initial Commit

---
+++ SkiDays/SkiDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkiDaysApp.swift
+//  SkiDays
+//
+//  Created by MacOS on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SkiDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
