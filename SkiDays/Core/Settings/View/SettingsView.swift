9e0ea5739865fb210946f2e394f7ce773955a82b
Michal Michalik <michalik515@gmail.com>
2022-09-14 16:39:02 +0200

    Show alert when signing out

--- SkiDays/Core/Settings/View/SettingsView.swift
+++ SkiDays/Core/Settings/View/SettingsView.swift
@@ -4,13 +4,21 @@ import Firebase
 struct SettingsView: View {
     
     @EnvironmentObject var viewModel: AuthViewModel
+    @State private var showingConfirmation = false
     
     var body: some View {
         ScrollView{
             VStack{
                 ThemeSettingsRow()
                 SettingsRow(image: "figure.walk", title: "Sign Out")
-                    .onTapGesture { viewModel.signOut() }
+                    .onTapGesture { showingConfirmation = true }
+                    .alert(isPresented: $showingConfirmation){
+                        Alert(
+                            title: Text("Sign Out ?"),
+                            primaryButton: .destructive(Text("Sign Out")){ viewModel.signOut() },
+                            secondaryButton: .cancel()
+                        )
+                    }
             }
         }
         .foregroundColor(.blackWhite)
