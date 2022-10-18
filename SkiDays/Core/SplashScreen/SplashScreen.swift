39c45a1589d2e6fd070e9e1489ecfa0966277bec
Michal Michalik <michalik515@gmail.com>
2022-10-18 16:24:13 +0200

    SplashScreen: Add app Icon, support dark mode

--- SkiDays/Core/SplashScreen/SplashScreen.swift
+++ SkiDays/Core/SplashScreen/SplashScreen.swift
@@ -6,19 +6,23 @@ struct SplashScreen: View {
     @State var endSplash = false
 
     var body: some View {
-        VStack(spacing: -45) {
-            Text("SKI")
-                .font(.system(size: 130).bold())
-            Text("DAYS")
-                .font(.system(size: 80).bold())
+//        VStack(spacing: -45) {
+//            Text("SKI")
+//                .font(.system(size: 130).bold())
+        //            Text("DAYS")
+        //                .font(.system(size: 80).bold())
+        //        }
+        ZStack {
+            Color.background
+            Image("AppLogo")
+                .resizable()
+                .frame(width: 200, height: 200)
+                .scaleEffect(animate ? 1 : 0)
+                .onAppear { animateSplash() }
+                .ignoresSafeArea()
         }
-        .scaleEffect(animate ? 1 : 0)
-        .onAppear { animateSplash() }
-        .foregroundColor(.blackWhite)
-        .ignoresSafeArea()
-        .frame(width: UIScreen.main.bounds.size.width,
-               height: UIScreen.main.bounds.size.height)
-        .background(Color.background)
+        .frame(width: UIScreen.main.bounds.width,
+               height: UIScreen.main.bounds.height)
     }
 
     private func animateSplash() {
