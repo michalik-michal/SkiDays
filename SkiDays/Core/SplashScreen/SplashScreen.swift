76fd7ea2a866ec272a6284777ec491c64f20f30c
Michal Michalik <michalik515@gmail.com>
2022-12-08 15:48:06 +0100

    SplashScreen: Update animation

--- SkiDays/Core/SplashScreen/SplashScreen.swift
+++ SkiDays/Core/SplashScreen/SplashScreen.swift
@@ -2,38 +2,24 @@ import SwiftUI
 
 struct SplashScreen: View {
 
-    @State var animate = false
-    @State var endSplash = false
+    @State private var opacity = 0.0
 
     var body: some View {
-//        VStack(spacing: -45) {
-//            Text("SKI")
-//                .font(.system(size: 130).bold())
-        //            Text("DAYS")
-        //                .font(.system(size: 80).bold())
-        //        }
         ZStack {
             Color.background
             Image("AppLogo")
                 .resizable()
                 .frame(width: 200, height: 200)
-                .scaleEffect(animate ? 1 : 0)
-                .onAppear { animateSplash() }
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation {
+                        opacity = 1.0
+                    }
+                }
                 .ignoresSafeArea()
         }
         .frame(width: UIScreen.main.bounds.width,
                height: UIScreen.main.bounds.height)
-    }
-
-    private func animateSplash() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
-            withAnimation(Animation.interactiveSpring()) {
-                animate.toggle()
-            }
-            withAnimation(Animation.interactiveSpring()) {
-                endSplash.toggle()
-            }
-        }
     }
 }
 
